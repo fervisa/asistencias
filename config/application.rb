13166214a2eb453b898f322389a64f5cb8f67462
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-11-02 09:24:37 -0600

    Rails config to serve .html.haml templates from assets dir

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Asistencias
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # adding assets path to haml templates (used by angular)
+    config.assets.paths << Rails.root.join('app', 'assets', 'templates')
   end
 end
